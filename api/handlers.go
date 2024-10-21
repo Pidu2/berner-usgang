cfb09958d58f62b32a1cbef0b7281fe0482ab1f1
Beat Schärz <beatschaerz@hotmail.com>
2024-10-21 20:55:51 +0200

    allow cors

--- api/handlers.go
+++ api/handlers.go
@@ -10,14 +10,21 @@ import (
 	"github.com/Pidu2/berner-usgang/globals"
 )
 
+func enableCORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")   // Allow localhost:8080
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE") // Specify allowed methods
+}
+
 func HandleGetScrapers(w http.ResponseWriter, r *http.Request) {
 	// Return as JSON
+	enableCORS(w)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(globals.AvailableScrapers)
 }
 
 // Handle the /scraper/<scraper> endpoint - calls the scraper function
 func HandleScrape(w http.ResponseWriter, r *http.Request) {
+	enableCORS(w)
 	// Extract the scraper name from the URL path
 	scraperName := r.URL.Path[len("/scraper/"):] // e.g., "dachstock", "gaskessel", "isc"
 
